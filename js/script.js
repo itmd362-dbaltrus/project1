995985facc5c75c92e84ab2c122265b86f0a90c7
DlayDlay <dylan.baltrus@gmail.com>
2021-10-17 18:18:08 -0500

    added radio focus color change

--- js/script.js
+++ js/script.js
@@ -6,14 +6,22 @@ function jschg(){
   h.setAttribute('class', 'hasjs');   
 }
 
-function color(){	
-	b.style.backgroundColor = 'orange';
-	b.style.color = 'black';
+function btnClick(){	
+  b.style.backgroundColor = 'orange';
+  b.style.color = 'black';
 	
-	var name = document.getElementsByName('fname_field')[0].value + ' ' + document.getElementsByName('lname_field')[0].value;
-    alert('The account for subscriber ' + name + ' has been created');
+  var name = document.getElementsByName('fname_field')[0].value + ' ' + document.getElementsByName('lname_field')[0].value;
+  alert('The account for subscriber ' + name + ' has been created');
+}
+
+function radiofocus(i){
+  document.getElementsByTagName('label')[i+4].style.color = 'orange';
 }
 
 
 window.addEventListener('load', jschg);
-b.addEventListener('click', color);
+b.addEventListener('click', btnClick);
+document.getElementsByName('news_field')[0].addEventListener('focus', function(){radiofocus(0);});
+document.getElementsByName('news_field')[1].addEventListener('focus', function(){radiofocus(1);});
+document.getElementsByName('news_field')[2].addEventListener('focus', function(){radiofocus(2);});
+document.getElementsByName('news_field')[3].addEventListener('focus', function(){radiofocus(3);});
