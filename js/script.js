2b023cad884e09d1e6dcadb43266fdd4750fc9d2
DlayDlay <dylan.baltrus@gmail.com>
2021-10-17 18:20:09 -0500

    added js radio blur function

--- js/script.js
+++ js/script.js
@@ -18,10 +18,20 @@ function radiofocus(i){
   document.getElementsByTagName('label')[i+4].style.color = 'orange';
 }
 
+function radioblur(i){
+  document.getElementsByTagName('label')[i+4].style.color = 'black';
+}
 
 window.addEventListener('load', jschg);
 b.addEventListener('click', btnClick);
+
 document.getElementsByName('news_field')[0].addEventListener('focus', function(){radiofocus(0);});
 document.getElementsByName('news_field')[1].addEventListener('focus', function(){radiofocus(1);});
 document.getElementsByName('news_field')[2].addEventListener('focus', function(){radiofocus(2);});
 document.getElementsByName('news_field')[3].addEventListener('focus', function(){radiofocus(3);});
+
+document.getElementsByName('news_field')[0].addEventListener('blur', function(){radioblur(0);});
+document.getElementsByName('news_field')[1].addEventListener('blur', function(){radioblur(1);});
+document.getElementsByName('news_field')[2].addEventListener('blur', function(){radioblur(2);});
+document.getElementsByName('news_field')[3].addEventListener('blur', function(){radioblur(3);});
+
